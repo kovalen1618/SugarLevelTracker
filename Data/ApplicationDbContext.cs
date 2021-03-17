b22e229603abb6a97b3c3af208554f2b021679e8
Ethan Kovalenko <ashervoronin@gmail.com>
2021-03-17 01:50:20 -0500

    Initialized repository.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using SugarLevelTracker.Models;
+
+namespace SugarLevelTracker.Data
+{
+    // DbContext refers to the connection string
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+            base("OktaConnectionString")
+        { 
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        // This will map to a table in the database
+        public DbSet<SugarLevel> SugarLevels { get; set; }
+    }
+}
